b0b60ba88084b2f42e8a2c740088592bff470675
Jayant Sinha <jayant.sinha@hotmail.com>
2019-03-25 04:56:39 +0530

    Added ping test for router

--- main_test.go
+++ main_test.go
@@ -4,31 +4,29 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
-func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
-	req, _ := http.NewRequest(method, path, nil)
-	w := httptest.NewRecorder()
-	r.ServeHTTP(w, req)
-	return w
-}
-
 func TestPing(t *testing.T) {
-	resp := gin.H{"message": "pong"}
-	router := setupRouter()
-
-	w := performRequest(router, http.MethodGet, "/ping")
-	assert.Equal(t, http.StatusOK, w.Code)
-
 	var response map[string]string
-	err := json.Unmarshal([]byte(w.Body.String()), &response)
-	value, exists := response["message"]
 
+	handler := func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
+	}
+
+	router := gin.New()
+	router.GET("/ping", handler)
+
+	req, _ := http.NewRequest(http.MethodGet, "/ping", nil)
+	resp := httptest.NewRecorder()
+	router.ServeHTTP(resp, req)
+	err := json.Unmarshal([]byte(resp.Body.String()), &response)
+	value, exists := response["message"]
 	assert.Nil(t, err)
 	assert.True(t, exists)
-	assert.Equal(t, resp["message"], value)
+	assert.Equal(t, "pong", value)
 
 }
