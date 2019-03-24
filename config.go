4bb05365cc7b286346430b5c6ee694c951bc5e86
Jayant Sinha <jayant.sinha@hotmail.com>
2019-03-25 02:01:32 +0530

    Added comments

--- config.go
+++ config.go
@@ -7,10 +7,18 @@ const (
 )
 
 var (
+	// Domain name of the http server
 	Domain = "localhost"
+	// ListenPort for gin http server
 	ListenPort = ":8888"
-	Environment = PRODUCTION
+	// Environment sets the gin mode
+	Environment = TEST
+	// Logging is a flag which will tell the server whether to enable logging or not
+	// enabled with log path, the log will be saved to the file specified in LoggingPath
 	Logging = false
-	LoggingPath = ""
+	// LoggingPath is the absolute path of the file to use for logging.
+	// Please set proper owner and permissions for the file
+	LoggingPath = "/var/log/web-crawler.log"
+	// Version of the web crawler
 	Version = "v1.0"
 )
